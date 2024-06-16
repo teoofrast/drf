c465fe2d4b719c96699e6ec7ac770ede724b9829
teoofrast <teoofrast@gmail.com>
2024-06-16 15:40:08 +0500

    add permissions

--- drfcatalog/drfcatalog/urls.py
+++ drfcatalog/drfcatalog/urls.py
@@ -8,15 +8,16 @@ from rest_framework_simplejwt.views import (
 from rest_framework import routers
 from magazine.views import *
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r'watch', WatchAPIViewSet)
 router.register(r'manufacturers', ManufacturerViewSet)
 router.register(r'basket', AddToCart)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/', include(router.urls)),
+    path('api/v1/drf-auth', include('rest_framework.urls')),
 ]
